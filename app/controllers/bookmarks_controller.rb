aad21c98d6a217e02af6cdfd746dfe417e73cbb2
Harikrishna <dostomkeliye@yahoo.com>
2015-12-27 00:29:42 -0600

    bookmarks can be created and saved

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,4 +4,21 @@ class BookmarksController < ApplicationController
 		@bookmarks = Bookmark.all
 	end
 
+	def new
+		@bookmark=Bookmark.new
+	end
+
+	def create
+		@bookmark=Bookmark.create(bookmark_params)
+		redirect_to action: :index
+	end
+
+
+
+	private
+	def bookmark_params
+		params.require(:bookmark).permit(:url, :title, :comment, :favorite)	
+	end
+
+
 end
